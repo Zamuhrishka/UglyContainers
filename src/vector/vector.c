3c8b56a21bdd1a3117b0bf50358a25e1f862e4d1
Alexander <roux@yandex.ru>
2021-08-04 00:18:50 +0300

    Update vector.c

--- src/vector/vector.c
+++ src/vector/vector.c
@@ -1,56 +1,48 @@
-/**********************************************************************************************
-* @file    		vector.c
-* @author  		Kovalchuk Alexander aka mz4
-* @email 		roux@yandex.ru
-* @brief		Vectors are sequence containers representing arrays that can change in size.
-* 				Internally, vectors use a dynamically allocated array to store their elements.
-* 				This array may need to be reallocated in order to grow in size when new elements
-* 				are inserted, which implies allocating a new array and moving all elements to it.
-* 				This is a relatively expensive task in terms of processing time, and thus, vectors
-* 				do not reallocate each time an element is added to the container.
-*
-* @warning 		This data structure is not thread-safe!
-***********************************************************************************************/
-//_____ I N C L U D E S _______________________________________________________________________
+/**
+* \file         vector.c
+* \author       Kovalchuk Alexander (roux@yandex.ru)
+* \brief        This file contains the prototypes functions which use for...
+*/
+//_____ I N C L U D E S _______________________________________________________
 #include "vector.h"
+
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
-//_____ D E F I N I T I O N ___________________________________________________________________
-//_____ V A R I A B L E   D E F I N I T I O N  ________________________________________________
-//! @brief Structure of vector
-//! @{
+#include <assert.h>
+//_____ C O N F I G S  ________________________________________________________
+#define RESIZE_FACTOR 						(32u)
+//_____ D E F I N I T I O N S _________________________________________________
+//< @brief Structure of vector
+//< @{
 struct _vector
 {
-	vDataType_t *data;																			//!pointer to the data array
-	size_t size;																				//!size of vector
-	size_t capacity;																			//!capacity of vector (available size)
+	uint8_t* data;																///< array of data
+    size_t size;																///< current size
+    size_t capacity;															///< max size
+    size_t esize;																///< size in bytes for one element
 };
-//! @}
-
-//!Pointer to the memory allocation function
+//< @}
+//_____ M A C R O S ___________________________________________________________
+//_____ V A R I A B L E S _____________________________________________________
+//< Pointer to the memory allocation function
 static void* (*vector_mem_alloc)(size_t sizemem) = NULL;
 
-//!Pointer to the memory free function
+//< Pointer to the memory free function
 static void (*vector_mem_free) (void *ptrmem) = NULL;
-//_____ I N L I N E   F U N C T I O N   D E F I N I T I O N   _________________________________
-/**
-* @brief 	Resize vector.
-*
-* @param  	*vector[in] - pointer to the resized vector.
-*
-* @return 	true/false.
-*/
+//_____ P R I V A T E  F U N C T I O N S_______________________________________
 __attribute__((always_inline)) inline static bool vector_resize(vector_t *vector)
 {
-	size_t new_size = vector->size + RESIZE_KOEFF;
+	size_t new_size = vector->size + RESIZE_FACTOR;
 
-	vDataType_t* tmp = (vDataType_t*) vector_mem_alloc(new_size);
+	uint8_t* tmp = (uint8_t*) vector_mem_alloc(new_size * vector->esize);
 	if (tmp == NULL) {
 		return false;
 	}
 
-	for(size_t i = 0; i < vector->size; i++) {
+	size_t size_in_bytes = vector->size * vector->esize;
+
+	for(size_t i = 0; i < size_in_bytes; i++) {
 		tmp[i] = vector->data[i];
 	}
 
@@ -61,72 +53,60 @@ __attribute__((always_inline)) inline static bool vector_resize(vector_t *vector
 
 	return true;
 }
-//_____ S T A T I Ñ  F U N C T I O N   D E F I N I T I O N   __________________________________
-//_____ F U N C T I O N   D E F I N I T I O N   _______________________________________________
-/**
-* @brief 	This function register memory allocation function.
-*
-* 			The registration function will be use for allocation memory
-* 			for vector store.
-*
-* 			@warning You must register allocation function before call any
-* 			function this library.
-*
-* 			@example vector_alloc_callback_reg(&malloc);
-*
-* @param  	*custom_malloc[in] - pointer to the memory allocation function.
-*
-* @return 	none.
-*/
-void vector_alloc_callback_reg(void* (*custom_malloc)(size_t sizemem))
+
+__attribute__((always_inline)) inline static bool is_callbacks_valid(void)
 {
+	return ((vector_mem_free != NULL) && (vector_mem_alloc != NULL)) ? true : false;
+}
+//_____ P U B L I C  F U N C T I O N S_________________________________________
+/**
+* This function used to register function for alloc memory.
+*
+* Public function defined in queue.h
+*/
+void vector_reg_mem_alloc_cb(void* (*custom_malloc)(size_t sizemem))
+{
+	assert(custom_malloc);
 	vector_mem_alloc = custom_malloc;
 }
 
 /**
-* @brief 	This function register memory free function.
+* This function used to register function for alloc memory.
 *
-* 			The registration function will be use for free memory
-* 			of vector.
-*
-* 			@warning You must register free function before call any
-* 			function this library.
-*
-* 			@example vector_free_callback_reg(&free);
-*
-* @param  	*custom_free[in] - pointer to the memory free function.
-*
-* @return 	none.
+* Public function defined in queue.h
 */
-void vector_free_callback_reg(void (*custom_free)(void * ptrmem))
+void vector_reg_mem_free_cb(void (*custom_free)(void * ptrmem))
 {
+	assert(custom_free);
 	vector_mem_free = custom_free;
 }
 
 /**
-* @brief 	Create new vector.
+* This function used to register function for alloc memory.
 *
-* @param  	size[in] - size of vector.
-*
-* @return 	pointer to  the creating vector - if ok.
-* 		  	NULL - if error occur.
+* Public function defined in queue.h
 */
-vector_t* vector_create(size_t size)
+vector_t* vector_create(size_t nbm, size_t esize)
 {
-	if(vector_mem_alloc == NULL || vector_mem_free == NULL) {
-		return NULL;
+	vector_t* vector = NULL;
+
+	if(!is_callbacks_valid()) {
+		return false;
 	}
 
-	vector_t *vector = (vector_t*) vector_mem_alloc(sizeof(vector_t));
+	vector = vector_mem_alloc(sizeof(*vector));
 	if (vector == NULL) {
 		return NULL;
 	}
 
 	vector->size = 0;
-	vector->capacity = size + RESIZE_KOEFF;
+	vector->capacity = nbm + RESIZE_FACTOR;
+	vector->esize = esize;
+	size_t size_in_bytes = vector->capacity * vector->esize;
 
-	vector->data = (vDataType_t*) vector_mem_alloc(vector->capacity);
-	if (vector->data == NULL) {
+	vector->data = (uint8_t*) vector_mem_alloc(size_in_bytes);
+	if (vector->data == NULL) 
+	{
 		vector_mem_free((void*)vector);
 		return NULL;
 	}
@@ -135,17 +115,13 @@ vector_t* vector_create(size_t size)
 }
 
 /**
-* @brief 	Delete vector.
+* This function used to register function for alloc memory.
 *
-* @param  	**vector[in] - pointer to the vector.
-*
-* @return 	none.
+* Public function defined in queue.h
 */
 void vector_delete(vector_t **vector)
 {
-	if(*vector == NULL) {
-		return;
-	}
+	assert(*vector);
 
 	vector_mem_free((*vector)->data);
 	vector_mem_free(*vector);
@@ -153,173 +129,229 @@ void vector_delete(vector_t **vector)
 }
 
 /**
-* @brief 	Check vector for empty state.
+* This function used to register function for alloc memory.
 *
-* @param  	*vector[in] - pointer to the vector.
-*
-* @return 	true - if vector empty and false - if no.
+* Public function defined in queue.h
 */
-bool vector_empty(const vector_t *vector)
+bool vector_is_empty(const vector_t *vector)
 {
+	assert(vector);
 	return (vector->size == 0) ? true : false;
 }
 
 /**
-* @brief 	Return size of vector.
+* This function used to check the queue for full.
 *
-* @param  	*vector[in] - pointer to the vector.
+* Public function defined in queue.h
+*/
+bool vector_is_full(const vector_t *vector)
+{
+	assert(vector);
+	return (vector->size == vector->capacity) ? true : false;
+}
+
+/**
+* This function used to register function for alloc memory.
 *
-* @return 	size of vector.
+* Public function defined in queue.h
 */
 size_t vector_size(const vector_t *vector)
 {
+	assert(vector);
 	return vector->size;
 }
 
 /**
-* @brief 	Return capacity of vector.
+* This function used to register function for alloc memory.
 *
-* @param  	*vector[in] - pointer to the vector.
-*
-* @return 	capacity of vector.
+* Public function defined in queue.h
 */
 size_t vector_capacity(const vector_t *vector)
 {
+	assert(vector);
 	return vector->capacity;
 }
 
 /**
-* @brief 	Clear vector.
+* This function return free size of queue.
 *
-* @param  	*vector[in] - pointer to the vector.
-*
-* @return 	true/false.
+* Public function defined in queue.h
 */
-bool vector_clear(vector_t *vector)
+size_t vector_free_space(const vector_t *vector)
 {
-	if(vector == NULL) {
-		return false;
-	}
+	assert(vector);
+	return vector->capacity - vector->size;
+}
 
-	vDataType_t* tmp = (vDataType_t*) vector_mem_alloc(RESIZE_KOEFF);
-	if (tmp == NULL) {
-		return false;
-	}
+/**
+* This function used to register function for alloc memory.
+*
+* Public function defined in queue.h
+*/
+bool vector_reset(vector_t *vector)
+{
+	assert(vector);
 
-	vector_mem_free(vector->data);
+	size_t size_in_bytes = vector->size * vector->esize;
 
-	vector->data = tmp;
 	vector->size = 0;
-	vector->capacity = RESIZE_KOEFF;
+
+	for(size_t i = 0; i < size_in_bytes; i++) {
+		vector->data[i] = 0;
+	}
 
 	return true;
 }
 
 /**
-* @brief 	This function read data from index position.
+* This function used to register function for alloc memory.
 *
-* @param  	*vector[in] - pointer to the vector.
-* @param  	*data[out] - read data.
-* @param  	index[in] - read position.
-*
-* @return 	true/false.
+* Public function defined in queue.h
 */
-bool vector_at(const vector_t *vector, vDataType_t *data, size_t index)
+bool vector_at(const vector_t *vector, void *data, size_t index)
 {
-	if(vector == NULL || vector->size == 0 || index > vector->size) {
+	assert(vector);
+	assert(data);
+
+	if(index > vector->size ||
+		vector_is_empty(vector)) 
+	{
 		return false;
 	}
 
-	*data = vector->data[index];
+	uint8_t* pData = (uint8_t*)data;
+	size_t size_in_bytes = vector->capacity * vector->esize;
+
+	size_t pos = index * vector->esize;
+	for(size_t i = 0; i < vector->esize; i++)
+	{
+		pData[i] = vector->data[pos + i];
+	}
+
 	return true;
 }
 
 /**
-* @brief 	This function read and delete data from back of vector.
+* This function used to register function for alloc memory.
 *
-* @param  	*vector[in] - pointer to the vector.
-* @param  	*data[out] - read data.
-*
-* @return 	true/false.
+* Public function defined in queue.h
 */
-bool vector_pop_back(vector_t *vector, vDataType_t *data)
+bool vector_pop_back(vector_t *vector, void *data)
 {
-	if(vector == NULL || vector->size == 0) {
+	assert(vector);
+	assert(data);
+
+	if(vector_is_empty(vector)) 
+	{
 		return false;
+	}
+
+	uint8_t* pData = (uint8_t*)data;
+	size_t size_in_bytes = vector->capacity * vector->esize;
+
+	size_t pos = vector->size * vector->esize;
+	for(size_t i = 0; i < vector->esize; i++)
+	{
+		pData[i] = vector->data[pos + i];
+		vector->data[pos + i] = 0;
 	}
 
 	vector->size--;
-	*data = vector->data[vector->size];
-	vector->data[vector->size] = 0;
 
 	return true;
 }
 
 /**
-* @brief 	This function read and delete data from index position of vector.
+* This function used to register function for alloc memory.
 *
-* @param  	*vector[in] - pointer to the vector.
-* @param  	*data[out] - read data.
-* @param  	index[in] - index position.
-*
-* @return 	true/false.
+* Public function defined in queue.h
 */
-bool vector_extract(vector_t *vector, vDataType_t *data, size_t index)
+bool vector_extract(vector_t *vector, void *data, size_t index)
 {
-	if(vector == NULL || vector->size == 0 || index > vector->size) {
+	assert(vector);
+	assert(data);
+
+	if(index > vector->size ||
+		vector_is_empty(vector)) 
+	{
 		return false;
 	}
 
-	*data = vector->data[index];
+	uint8_t* pData = (uint8_t*)data;
+	size_t size_in_bytes = vector->size * vector->esize;
+
+	size_t pos = index * vector->esize;
+	for(size_t i = 0; i < vector->esize; i++)
+	{
+		pData[i] = vector->data[pos + i];
+	}
+
 	vector->size--;
 
-	for(size_t i = index; i < vector->size; i++) {
-		vector->data[i] = vector->data[i+1];
+	for(size_t i = pos; i < size_in_bytes; i++)
+	{
+		vector->data[i] = vector->data[pos + i + 1];
 	}
 
 	return true;
 }
 
 /**
-* @brief 	This function read and delete data from front of vector.
+* This function used to register function for alloc memory.
 *
-* @param  	*vector[in] - pointer to the vector.
-* @param  	*data[out] - read data.
-*
-* @return 	true/false.
+* Public function defined in queue.h
 */
-bool vector_pop_front(vector_t *vector, vDataType_t *data)
+bool vector_pop_front(vector_t *vector, void *data)
 {
-	if(vector == NULL || vector->size == 0) {
+	assert(vector);
+	assert(data);
+
+	if(vector_is_empty(vector)) 
+	{
 		return false;
 	}
 
-	*data = vector->data[0];
+	uint8_t* pData = (uint8_t*)data;
+	size_t size_in_bytes = vector->capacity * vector->esize;
+
+	size_t pos = 0;
+	for(size_t i = 0; i < vector->esize; i++)
+	{
+		pData[i] = vector->data[pos + i];
+		vector->data[pos + i] = 0;
+	}
+
 	vector->size--;
 
-	for(size_t i = 0; i < vector->size; i++) {
-		vector->data[i] = vector->data[i+1];
+	for(size_t i = pos; i < size_in_bytes; i++)
+	{
+		vector->data[i] = vector->data[pos + i + 1];
 	}
 
 	return true;
 }
 
 /**
-* @brief 	This function delete data from index position.
+* This function used to register function for alloc memory.
 *
-* @param  	*vector[in] - pointer to the vector.
-* @param  	*data[in] - read data.
-*
-* @return 	true/false.
+* Public function defined in queue.h
 */
 bool vector_erase(vector_t *vector, size_t index)
 {
-	if(vector == NULL || vector->size == 0 || index > vector->size) {
+	assert(vector);
+
+	if(index > vector->size ||
+		vector_is_empty(vector)) 
+	{
 		return false;
 	}
 
-	for(size_t i = index; i < vector->size; i++) {
-		vector->data[i] = vector->data[i+1];
+	size_t size_in_bytes = vector->size * vector->esize;
+	size_t pos = index * vector->esize;
+
+	for(size_t i = pos; i < size_in_bytes; i++)
+	{
+		vector->data[i] = vector->data[pos + i + 1];
 	}
 
 	vector->size--;
@@ -328,59 +360,69 @@ bool vector_erase(vector_t *vector, size_t index)
 }
 
 /**
-* @brief 	This function added data into end of vector.
+* This function used to register function for alloc memory.
 *
-* @param  	*vector[in] - pointer to the vector.
-* @param  	data[in] - data.
-*
-* @return 	true/false.
+* Public function defined in queue.h
 */
-bool vector_append(vector_t *vector, vDataType_t data)
+bool vector_append(vector_t *vector, void *data)
 {
-	if(vector == NULL) {
-		return false;
-	}
+	assert(vector);
+	assert(data);
 
-	if(vector->size >= vector->capacity)
+	if(vector_is_full(vector))
 	{
 		if(vector_resize(vector) != true) {
 			return false;
 		}
 	}
 
-	vector->data[vector->size] = data;
+	uint8_t* pData = (uint8_t*)data;
+	size_t pos = vector->size * vector->esize;
+
+	for(size_t i = 0; i < vector->esize; i++)
+	{
+		vector->data[pos + i] = pData[i];
+	}
 	vector->size++;
+
 	return true;
 }
 
 /**
-* @brief 	This function added data into index position into vector.
+* This function used to register function for alloc memory.
 *
-* @param  	*vector[in] - pointer to the vector.
-* @param  	data[in] - data.
-* @param  	index[in] - position.
-*
-* @return 	true/false.
+* Public function defined in queue.h
 */
-bool vector_insert(vector_t *vector, vDataType_t data, size_t index)
+bool vector_insert(vector_t *vector, void *data, size_t index)
 {
-	if(vector == NULL || index > vector->size) {
+#if 0
+	assert(vector);
+	assert(data);
+
+	if(index > vector->size)
+	{
 		return false;
 	}
 
-	if(vector->size >= vector->capacity)
+	if(vector_is_full(vector))
 	{
 		if(vector_resize(vector) != true) {
 			return false;
 		}
 	}
 
-	for(int32_t i = vector->size; i >= (int32_t)index; --i) {
+
+
+
+	for(int32_t i = vector->size * vector->esize; i >= (int32_t)index * vector->esize; --i) {
 		vector->data[i+1] = vector->data[i];
 	}
 
 	vector->data[index] = data;
 	vector->size++;
-	return true;
 
+	return true;
+#else
+	return false;
+#endif
 }
