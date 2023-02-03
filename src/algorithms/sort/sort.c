95eefbc68e2388cf91d31b0f0cf34685619ea717
Alexander <roux@yandex.ru>
2023-02-03 17:36:48 +0100

    Add draft sort alg.

---
+++ src/algorithms/sort/sort.c
@@ -0,0 +1,24 @@
+/**
+ * @file sort.c
+ * @author Aleksander Kovalchuk (aliaksander.kavalchuk@gmail.com)
+ * @brief
+ * @date 2023-01-26
+ */
+
+//_____ I N C L U D E S _______________________________________________________
+#include "sort.h"
+//_____ C O N F I G S  ________________________________________________________
+
+//_____ D E F I N I T I O N S _________________________________________________
+
+//_____ M A C R O S ___________________________________________________________
+
+//_____ V A R I A B L E S _____________________________________________________
+
+//_____ P R I V A T E  F U N C T I O N S_______________________________________
+
+//_____ P U B L I C  F U N C T I O N S_________________________________________
+void sort(container_t* container, sort_dir_e direction, cmp_t (cmp*)(void*, void*))
+{
+
+}
