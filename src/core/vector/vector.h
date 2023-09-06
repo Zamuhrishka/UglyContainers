6cb4477ee6776b6cdb6043ff28e37c9efed6f521
Alexander <roux@yandex.ru>
2023-09-07 00:15:50 +0200

    Apply clang-format to few files

--- src/core/vector/vector.h
+++ src/core/vector/vector.h
@@ -19,23 +19,23 @@ typedef struct Private_tag private_t;
 
 typedef struct
 {
-    private_t* private;
-    struct
-    {
-        bool (*resize)(void* this, size_t new_size);
-        bool (*push_front)(void* this, const void* data);
-        bool (*pop_front)(void* this, void* data);
-        bool (*push_back)(void* this, const void* data);
-        bool (*pop_back)(void* this, void* data);
-        bool (*insert)(void* this, const void* data, size_t index);
-        bool (*extract)(void* this, void* data, size_t index);
-        bool (*replace)(void* this, const void* data, size_t index);
-        bool (*at)(const void* this, void* data, size_t index);
-        bool (*erase)(void* this, size_t index);
-        void* (*peek)(void* this, size_t index);
-        bool (*clear)(void* this);
-        size_t (*size)(const void* this);
-    };
+  private_t* private;
+  struct
+  {
+    bool (*resize)(void* this, size_t new_size);
+    bool (*push_front)(void* this, const void* data);
+    bool (*pop_front)(void* this, void* data);
+    bool (*push_back)(void* this, const void* data);
+    bool (*pop_back)(void* this, void* data);
+    bool (*insert)(void* this, const void* data, size_t index);
+    bool (*extract)(void* this, void* data, size_t index);
+    bool (*replace)(void* this, const void* data, size_t index);
+    bool (*at)(const void* this, void* data, size_t index);
+    bool (*erase)(void* this, size_t index);
+    void* (*peek)(void* this, size_t index);
+    bool (*clear)(void* this);
+    size_t (*size)(const void* this);
+  };
 } vector_t;
 //_____ M A C R O S ___________________________________________________________
 
