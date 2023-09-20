a413bb2692f4204d97593c913699d91dfc7e8795
Alexander <roux@yandex.ru>
2023-09-20 21:07:33 +0200

    Update

--- src/core/vector/vector.h
+++ src/core/vector/vector.h
@@ -13,9 +13,9 @@
 #pragma once
 
 //_____ I N C L U D E S _______________________________________________________
-#include "stdbool.h"
-#include "stdint.h"
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 //_____ C O N F I G S  ________________________________________________________
 //_____ D E F I N I T I O N S _________________________________________________
 struct Private_tag;
