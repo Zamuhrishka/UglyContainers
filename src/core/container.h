80dfe7b128edbaadc01b5c0bf225e72c05ccec9f
Alexander <roux@yandex.ru>
2023-09-23 23:13:49 +0200

    Update

--- src/core/container.h
+++ src/core/container.h
@@ -18,13 +18,11 @@
 #pragma once
 
 //_____ I N C L U D E S _______________________________________________________
-#include "config.h"
-
-// #include "linked_list/linked_list.h"
-// #include "vector/vector.h"
 #include "interface/allocator_if.h"
-//_____ C O N F I G S  ________________________________________________________
 
+#include <stdbool.h>
+#include <stddef.h>
+//_____ C O N F I G S  ________________________________________________________
 //_____ D E F I N I T I O N S _________________________________________________
 struct Container_tag;
 typedef struct Container_tag container_t;
@@ -58,14 +56,14 @@ void container_alloc_init(allocate_fn_t alloc_cb, free_fn_t free_cb);
  * \param type Type of the container to be created.
  * \return Pointer to the newly created container or NULL if creation failed.
  */
-container_t* container_create(size_t esize, container_type_e type);
+container_t *container_create(size_t esize, container_type_e type);
 
 /**
  * \brief Frees up the memory associated with the container.
  *
  * \param container Double pointer to the container to be deleted.
  */
-void container_delete(container_t** container);
+void container_delete(container_t **container);
 
 /**
  * \brief Resizes the container to a new size.
@@ -74,7 +72,7 @@ void container_delete(container_t** container);
  * \param new_size The new size for the container.
  * \return true if resizing was successful, false otherwise.
  */
-bool container_resize(container_t* container, size_t new_size);
+bool container_resize(container_t *container, size_t new_size);
 
 /**
  * \brief Pushes an element to the front of the container.
@@ -83,7 +81,7 @@ bool container_resize(container_t* container, size_t new_size);
  * \param data Pointer to the data to be pushed.
  * \return true if the operation was successful, false otherwise.
  */
-bool container_push_front(container_t* container, const void* data);
+bool container_push_front(container_t *container, const void *data);
 
 /**
  * \brief Pops an element from the front of the container.
@@ -92,7 +90,7 @@ bool container_push_front(container_t* container, const void* data);
  * \param data Pointer to store the popped data.
  * \return true if the operation was successful, false otherwise.
  */
-bool container_pop_front(container_t* container, void* data);
+bool container_pop_front(container_t *container, void *data);
 
 /**
  * \brief Pushes an element to the back of the container.
@@ -101,7 +99,7 @@ bool container_pop_front(container_t* container, void* data);
  * \param data Pointer to the data to be pushed.
  * \return true if the operation was successful, false otherwise.
  */
-bool container_push_back(container_t* container, const void* data);
+bool container_push_back(container_t *container, const void *data);
 
 /**
  * \brief Pops an element from the back of the container.
@@ -110,7 +108,7 @@ bool container_push_back(container_t* container, const void* data);
  * \param data Pointer to store the popped data.
  * \return true if the operation was successful, false otherwise.
  */
-bool container_pop_back(container_t* container, void* data);
+bool container_pop_back(container_t *container, void *data);
 
 /**
  * \brief Inserts an element at a specified index in the container.
@@ -120,7 +118,7 @@ bool container_pop_back(container_t* container, void* data);
  * \param index The index at which the data should be inserted.
  * \return true if the operation was successful, false otherwise.
  */
-bool container_insert(container_t* container, const void* data, size_t index);
+bool container_insert(container_t *container, const void *data, size_t index);
 
 /**
  * \brief Extracts an element from a specified index in the container.
@@ -130,7 +128,7 @@ bool container_insert(container_t* container, const void* data, size_t index);
  * \param index The index from which the data should be extracted.
  * \return true if the operation was successful, false otherwise.
  */
-bool container_extract(container_t* container, void* data, size_t index);
+bool container_extract(container_t *container, void *data, size_t index);
 
 /**
  * \brief Replaces an element at a specified index in the container.
@@ -140,7 +138,7 @@ bool container_extract(container_t* container, void* data, size_t index);
  * \param index The index at which the data should be replaced.
  * \return true if the operation was successful, false otherwise.
  */
-bool container_replace(container_t* container, const void* data, size_t index);
+bool container_replace(container_t *container, const void *data, size_t index);
 
 /**
  * \brief Retrieves an element from a specified index in the container.
@@ -150,7 +148,7 @@ bool container_replace(container_t* container, const void* data, size_t index);
  * \param index The index from which the data should be retrieved.
  * \return true if the operation was successful, false otherwise.
  */
-bool container_at(const container_t* container, void* data, size_t index);
+bool container_at(const container_t *container, void *data, size_t index);
 
 /**
  * \brief Erases an element at a specified index in the container.
@@ -159,7 +157,7 @@ bool container_at(const container_t* container, void* data, size_t index);
  * \param index The index at which the element should be erased.
  * \return true if the operation was successful, false otherwise.
  */
-bool container_erase(container_t* container, size_t index);
+bool container_erase(container_t *container, size_t index);
 
 /**
  * \brief Peeks at an element at a specified index in the container without removing it.
@@ -168,7 +166,7 @@ bool container_erase(container_t* container, size_t index);
  * \param index The index from which the data should be peeked.
  * \return Pointer to the peeked data or NULL.
  */
-void* container_peek(const container_t* container, size_t index);
+void *container_peek(const container_t *container, size_t index);
 
 /**
  * \brief Clears all the elements from the container.
@@ -176,7 +174,7 @@ void* container_peek(const container_t* container, size_t index);
  * \param container Pointer to the container.
  * \return true if the operation was successful, false otherwise.
  */
-size_t container_clear(const container_t* container);
+size_t container_clear(const container_t *container);
 
 /**
  * \brief Returns the number of elements in the container.
@@ -184,4 +182,4 @@ size_t container_clear(const container_t* container);
  * \param container Pointer to the container.
  * \return Number of elements in the container.
  */
-size_t container_size(const container_t* container);
+size_t container_size(const container_t *container);
